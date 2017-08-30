073c671597cd59240e276b76ed66ac7aa03b6f42
Alex Ellis <alexellis2@gmail.com>
2017-08-30 10:27:36 +0100

    Initial fast-fork implementation of HTTP parser
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ com/openfaas/Parser.java
@@ -0,0 +1,31 @@
+package com.openfaas;
+
+import java.io.*;
+
+public class Parser {
+    public void acceptIncoming(DataInputStream dataStream, BufferedWriter out, HeaderReader parser) throws IOException {
+
+        StringBuffer rawHeader = parser.readHeader();
+        System.err.println(rawHeader);
+
+        HttpHeader header = new HttpHeader(rawHeader.toString());
+
+        if(header.getMethod() != null) {
+            System.err.println(header.getMethod() + " method");
+            System.err.println(header.getContentLength()  + " bytes");
+            
+            byte[] body = header.readBody(dataStream);
+
+            function.Handler handler = new function.Handler();
+            String functionResponse = handler.function(new String(body), header.getMethod());
+            
+            HttpResponse response = new HttpResponse();
+            response.setStatus(200);
+            response.setContentType("text/plain");
+            StringBuffer outBuffer = response.serialize(functionResponse);
+
+            out.write(outBuffer.toString(), 0, outBuffer.length());
+            out.flush();
+        }
+    }
+}
